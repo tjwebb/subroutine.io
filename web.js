5cec33676160dac2e9b5fab54c66fcf5cbddedd9
tjwebb <travis@traviswebb.com>
2013-08-25 14:39:55 -0400

    cleaned node_modules; added web.js and package.json

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
-var express = require("express");
+var util      = require('util'),
+  express     = require('express'),
+  subroutine  = require('./routes/subroutine.js'),
+  color       = require('cli-color'),
+  db          = require('./conf/db.js');
+
+process.on('uncaughtException', function(err) {
+  util.log(color.red('[main] uncaught exception: '+ err.stack));
+});
+
+db.init();
+
 var app = express();
 app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
 
-app.get('/', function(request, response) {
-      response.send(request);
-});
+app.post('/', subroutine.post);
+app.get ('/:hash', subroutine.get);
+app.put ('/:hash', subroutine.put);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-      console.log("Listening on " + port);
+  console.log('Listening on port ' + port);
 });
